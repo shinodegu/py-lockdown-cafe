823155deb22ad58ec6abe1d6aeb32c5a36c26907
Ihor <andreiunderwhat@mail.com>
2024-11-29 21:39:06 +0200

    'Solution'

--- app/cafe.py
+++ app/cafe.py
@@ -12,15 +12,15 @@ class Cafe:
         count = 0
         if "vaccine" not in visitor:
             count += 1
-            raise NotVaccinatedError("NotVaccinatedError")
+            raise NotVaccinatedError("The client does not have a vaccine")
 
         if visitor["vaccine"]["expiration_date"] < date.today():
             count += 1
-            raise OutdatedVaccineError("OutdatedVaccineError")
+            raise OutdatedVaccineError("The client's vaccine is expired")
 
         if visitor["wearing_a_mask"] is False:
             count += 1
-            raise NotWearingMaskError("NotWearingMaskError")
+            raise NotWearingMaskError("The client does not have a mask")
 
         if count == 0:
             return f"Welcome to {self.name}"
