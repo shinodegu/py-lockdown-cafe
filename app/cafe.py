815840ea23ca032d097f501c15d9462620bfab33
Ihor <andreiunderwhat@mail.com>
2024-11-29 21:34:51 +0200

    'Solution'

---
+++ app/cafe.py
@@ -0,0 +1,26 @@
+from app.errors import (NotVaccinatedError,
+                        OutdatedVaccineError,
+                        NotWearingMaskError)
+from datetime import date
+
+
+class Cafe:
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+    def visit_cafe(self, visitor: dict) -> str:
+        count = 0
+        if "vaccine" not in visitor:
+            count += 1
+            raise NotVaccinatedError("NotVaccinatedError")
+
+        if visitor["vaccine"]["expiration_date"] < date.today():
+            count += 1
+            raise OutdatedVaccineError("OutdatedVaccineError")
+
+        if visitor["wearing_a_mask"] is False:
+            count += 1
+            raise NotWearingMaskError("NotWearingMaskError")
+
+        if count == 0:
+            return f"Welcome to {self.name}"
--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+from app.errors import (VaccineError, NotWearingMaskError)
+from app.cafe import Cafe
+
+
+def go_to_cafe(friends: list, cafe: "Cafe") -> str:
+    not_wearing_mask_count = 0
+    for friend in friends:
+        try:
+            cafe.visit_cafe(friend)
+        except VaccineError:
+            return "All friends should be vaccinated"
+        except NotWearingMaskError:
+            not_wearing_mask_count += 1
+
+    if not_wearing_mask_count > 0:
+        return f"Friends should buy {not_wearing_mask_count} masks"
+
+    return f"Friends can go to {cafe.name}"
